93987334144cc039670b5ae0f65b940503e539af
Barabas Raffai <barabas.raffai@gmail.com>
2024-03-10 15:01:33 +0000

    WIP benchmark

--- main/app_main.c
+++ main/app_main.c
@@ -1,5 +1,5 @@
-#include "esp_log.h"
 #include "esp_dsp.h"
+#include "esp_log.h"
 #define USE_FULL_REPORT
 #include "report.inc"
 #include "sdkconfig.h"
@@ -45,21 +45,16 @@ void app_main(void) {
     abort();
   }
 
-  fir_f32_t fir1;
-  dsps_fir_init_f32(&fir1, data1, data2, 256);
-
-  fir_f32_t fir2;
-  dsps_fird_init_f32(&fir2, data1, data2, 256, 4);
-
-  float coeffs[5];
-  dsps_biquad_gen_lpf_f32(coeffs, 0.1, 1);
-
 #if CONFIG_IDF_TARGET_ESP32
   REPORT_HEADER_ESP32();
 #elif CONFIG_IDF_TARGET_ESP32S3
   REPORT_HEADER_ESP32S3();
 #endif
   REPORT_SECTION("**FFTs Radix-2 32 bit Floating Point**");
+
+  // TODO include bitrev here, the esp32_fft library includes it implicitly
+  // TODO add real FFT tests, including the cplx2real call which is implicit in
+  // esp32_fft
 
   REPORT_BENCHMARK("dsps_fft2r_fc32 for  64 complex points", dsps_fft2r_fc32,
                    dsps_fft2r_fc32_ansi, data1, 64);
@@ -86,6 +81,24 @@ void app_main(void) {
 
   REPORT_BENCHMARK("dsps_fft4r_fc32 for 1024 complex points", dsps_fft4r_fc32,
                    dsps_fft4r_fc32_ansi, data1, 1024);
+
+  REPORT_SECTION("**ESP32 FFTs 32 bit Floating Point**");
+
+  // TODO call esp32_fft library
+  REPORT_BENCHMARK_ANSI("dsps_fft2r_fc32 for  64 complex points",
+                        dsps_fft2r_fc32_ansi, data1, 64);
+
+  REPORT_BENCHMARK_ANSI("dsps_fft2r_fc32 for 128 complex points",
+                        dsps_fft2r_fc32_ansi, data1, 128);
+
+  REPORT_BENCHMARK_ANSI("dsps_fft2r_fc32 for 256 complex points",
+                        dsps_fft2r_fc32_ansi, data1, 256);
+
+  REPORT_BENCHMARK_ANSI("dsps_fft2r_fc32 for 512 complex points",
+                        dsps_fft2r_fc32_ansi, data1, 512);
+
+  REPORT_BENCHMARK_ANSI("dsps_fft2r_fc32 for 1024 complex points",
+                        dsps_fft2r_fc32_ansi, data1, 1024);
 
   dsps_fft2r_deinit_fc32();
   dsps_fft4r_deinit_fc32();
